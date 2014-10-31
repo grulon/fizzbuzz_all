94ac6100164a0bfb93512cae605e661a058f5d95
Gavin Rulon <gavin.rulon@icloud.com>
2014-10-31 17:16:11 -0400

    v2 final

--- FizzBuzzV2.py
+++ FizzBuzzV2.py
@@ -2,7 +2,7 @@ import sys
 
 def fizzbuzz(n):
 	"This calcs Mod 3, 5, 15 on number passed to it and determines if Fizz, Buzz, Fizzbuzz, or blank is returned"
-	#print 'n in Fizzbuzz function', n
+	
 	if n % 15 == 0:
 		return 'FizzBuzz'
 	elif n % 5 == 0:
@@ -10,8 +10,29 @@ def fizzbuzz(n):
 	elif n % 3 == 0:
 		return 'Fizz'
 	else:
-		return ''
+		return n
 
+def getinput()	:
+	var = 0
+	var = raw_input("Please enter a number: ")
+	try:
+		var = int(var)
+		return var
+	except:
+		var = getinput()
+		return var
+	
+x = sys.argv[1]
+
+try:
+	x = int(x)
+except:
+	print 'You didn''t pass in a number, please try again: '
+	x = int(getinput())
+finally:
+	print fizzbuzz(x)
+	
+	
 """  
 # used for Function testing
 testvalues = {3:'Fizz', 5:'Buzz', 15:'FizzBuzz'}
