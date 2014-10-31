fd7193b4f2ba25e507783f938c3feb2782663b3f
Gavin Rulon <gavin.rulon@icloud.com>
2014-10-31 13:09:21 -0400

    V1 complete

--- FizzBuzzV1.py
+++ FizzBuzzV1.py
@@ -1,6 +1,3 @@
-##Write a program that prints the numbers from 1 to 100. 
-##But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". 
-##For numbers which are multiples of both three and five print "FizzBuzz".
 
 for num in range(1, 101):
 	if num % 15 == 0:
@@ -11,17 +8,6 @@ for num in range(1, 101):
 		print "Fizz"
 	else:  
 		print num
-def fizzbuzz(n):
-	print 'n in Fizzbuzz function', n
-	return 'Fizz'
+		
 
-testvalues = {3:'Fizz', 5:'Buzz', 15:'FizzBuzz'}
-print 'GOCOLTS*********************'
-
-for k in testvalues:
-	print k
-	print 'testvalue should be:',testvalues[k]  
-	print 'fizzbuzz output:', fizzbuzz(k)
-	assert testvalues[k] ==  fizzbuzz(k)
-	
 	
--- FizzBuzzV2.py
+++ FizzBuzzV2.py
@@ -1,22 +1,22 @@
 def fizzbuzz(n):
-	print 'n in Fizzbuzz function', n
+	"This calcs Mod 3, 5, 15 on number passed to it and determines if Fizz, Buzz, Fizzbuzz, or blank is returned"
+	#print 'n in Fizzbuzz function', n
 	if n % 15 == 0:
 		return 'FizzBuzz'
 	elif n % 5 == 0:
 		return 'Buzz'
 	elif n % 3 == 0:
 		return 'Fizz'
-	else:  
+	else:
 		return ''
 
-
+"""  
+# used for Function testing
 testvalues = {3:'Fizz', 5:'Buzz', 15:'FizzBuzz'}
-print 'GOCOLTS*********************'
 
 for k in testvalues:
 	print k
-	print 'testvalue should be:',testvalues[k]  
+	print 'testvalue should be:',testvalues[k]
 	print 'fizzbuzz output:', fizzbuzz(k)
 	assert testvalues[k] ==  fizzbuzz(k)
-	
-	
+"""
