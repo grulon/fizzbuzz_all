66648f68e6a7ef607c78203917be4b514cb24e96
Gavin <grulon@users.noreply.github.com>
2014-10-31 17:27:02 -0400

    Update FizzBuzzV1.py

--- FizzBuzzV1.py
+++ FizzBuzzV1.py
@@ -1,8 +1,10 @@
-##Write a program that prints the numbers from 1 to 100. 
-##But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". 
-##For numbers which are multiples of both three and five print "FizzBuzz".
 
-for num in range(1, 101):
+# hard coded upper limit
+
+x = 100
+
+print 'FizzBuzz counting up to', x
+for num in range(1,x+1):
 	if num % 15 == 0:
 		print"FizzBuzz"
 	elif num % 5 == 0:
@@ -11,17 +13,4 @@ for num in range(1, 101):
 		print "Fizz"
 	else:  
 		print num
-def fizzbuzz(n):
-	print 'n in Fizzbuzz function', n
-	return 'Fizz'
-
-testvalues = {3:'Fizz', 5:'Buzz', 15:'FizzBuzz'}
-print 'GOCOLTS*********************'
-
-for k in testvalues:
-	print k
-	print 'testvalue should be:',testvalues[k]  
-	print 'fizzbuzz output:', fizzbuzz(k)
-	assert testvalues[k] ==  fizzbuzz(k)
-	
 	
